92ecfcfa62f5ce09380bf33bfa483ce5a79e4823
mpohler1 <masonpohler@gmail.com>
2021-01-24 13:34:27 -0600

    Implemented MailAPIService

--- src/main/java/AmazonLinkChecker.java
+++ src/main/java/AmazonLinkChecker.java
@@ -1,6 +1,9 @@
 import commandline.ApacheCommandLineHandler;
 import commandline.CommandLineHandler;
 import commandline.CommandLineHandlerException;
+import mail.MailAPIService;
+import mail.MailService;
+import mail.MailServiceException;
 import model.TaskModel;
 import webpage.JsoupWebpageElementChecker;
 import webpage.WebpageElementChecker;
@@ -9,6 +12,7 @@ import webpage.WebpageElementSelectorException;
 public class AmazonLinkChecker {
 
     private static final CommandLineHandler COMMAND_LINE_HANDLER = new ApacheCommandLineHandler();
+    private static final MailService MAIL_SERVICE = new MailAPIService();
     private static final String AMAZON_ADD_TO_CART_BUTTON_ID = "add-to-cart-button";
 
     public static void main(String[] args) {
@@ -19,14 +23,20 @@ public class AmazonLinkChecker {
             COMMAND_LINE_HANDLER.printHelp();
         } catch (WebpageElementSelectorException e) {
             e.printStackTrace();
+        } catch (MailServiceException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void doTask(TaskModel taskModel) throws WebpageElementSelectorException {
+    private static void doTask(TaskModel taskModel) throws WebpageElementSelectorException, MailServiceException {
         WebpageElementChecker elementChecker = new JsoupWebpageElementChecker();
 
         if (elementChecker.elementDoesExist(taskModel.getLink(), AMAZON_ADD_TO_CART_BUTTON_ID)) {
-
+            MAIL_SERVICE.sendMail(
+                    taskModel.getEmailSenderName(),
+                    taskModel.getEmailSenderAddress(),
+                    taskModel.getName() + " is back in stock.\n\n" + taskModel.getLink()
+            );
         }
     }
 }
