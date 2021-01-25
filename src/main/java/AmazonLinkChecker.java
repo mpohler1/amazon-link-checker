7118f13d9114a5ba50fac276954002bff4efada9
mpohler1 <masonpohler@gmail.com>
2021-01-24 22:59:52 -0600

    added print statements for simple logging

--- src/main/java/AmazonLinkChecker.java
+++ src/main/java/AmazonLinkChecker.java
@@ -9,6 +9,8 @@ import webpage.JsoupWebpageElementChecker;
 import webpage.WebpageElementChecker;
 import webpage.WebpageElementSelectorException;
 
+import java.util.Date;
+
 public class AmazonLinkChecker {
 
     private static final CommandLineHandler COMMAND_LINE_HANDLER = new ApacheCommandLineHandler();
@@ -20,6 +22,7 @@ public class AmazonLinkChecker {
             TaskModel taskModel = COMMAND_LINE_HANDLER.parseCommandLine(args);
             doTask(taskModel);
         } catch (CommandLineHandlerException e) {
+            e.printStackTrace();
             COMMAND_LINE_HANDLER.printHelp();
         } catch (WebpageElementSelectorException e) {
             e.printStackTrace();
@@ -33,13 +36,15 @@ public class AmazonLinkChecker {
 
         // If the Amazon product page has an 'add to cart' button, then the product is in stock
         if (elementChecker.elementDoesExist(taskModel.getProductPageLink(), AMAZON_ADD_TO_CART_BUTTON_ID)) {
+            System.out.println("An add to cart button was found on " + new Date().toString());
             MAIL_SERVICE.sendMail(
                     taskModel.getEmailSenderName(),
                     taskModel.getEmailSenderAddress(),
                     taskModel.getProductName() + " is back in stock.\n\n" + taskModel.getProductPageLink()
             );
+            System.out.println("Sent email on " + new Date().toString());
         } else {
-            // System.out.println("No add to cart button");
+            System.out.println("No add to cart button was found on " + new Date().toString());
         }
     }
 }
